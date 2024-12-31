f0faf9071ce5ebb042b2118a8baa208d1a63aba6
Rodrigo Llull Torres <rllullt@gmail.com>
2024-12-31 03:01:41 -0300

    Changed router in main for managing data

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, Routes, Route, RouterProvider } from 'react-router-dom'
 import './styles/index.css'
 import App from './routes/App.jsx'
-import Pokedex from './routes/Pokedex.jsx'
 import Pokegrid from './routes/Pokegrid.jsx'
+import Pokedex, { loader as pokedexLoader } from './routes/Pokedex'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "pokegrid",
+    element: <Pokegrid />,
+  },
+  {
+    path: "pokedex/:pokemonId",  // the ‘:’ tells that the route is going to be dinamic
+    element: <Pokedex />,
+    loader: pokedexLoader,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pokegrid" element={<Pokegrid />}/>
-        <Route path="/pokedex/:pokemonId" element={<Pokedex />}/>  {/* the ‘:’ tells that the route is going to be dinamic */}
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
