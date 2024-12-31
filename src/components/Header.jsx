37a0c07e9d6a343f577daebc091e6161cabd7bd6
Rodrigo Llull Torres <rllullt@gmail.com>
2024-12-30 23:14:04 -0300

    Hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import pokedexLogo from '../assets/pokedex_logo.png'
 
 function Header() {
     const navigate = useNavigate();
+    const [openNavigation, setOpenNavigation] = useState(false);
+
+    const toggleNavigation = () => {
+        if (openNavigation)
+            setOpenNavigation(false)
+        else
+            setOpenNavigation(true);
+    }
 
     const navigationElems = [
         {
@@ -17,14 +26,14 @@ function Header() {
         },
     ]
     return (
-        <div className="fixed top-0 z-50 backdrop-blur-sm border-b">
-            <div className="flex items-center px-5">
-                <button className="block w-{12rem}" onClick={() => { navigate('/'); }}>
+        <div className={`fixed top-0 left-0 w-full z-50 backdrop-blur-sm border-b border-n-6 bg-n-8/90 lg:bg-n-8/90 lg:backdrop-blur-sm`}>
+            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+                <button className="block w-{12rem} xl:mr-8" onClick={() => { navigate('/'); }}>
                     <img src={pokedexLogo} width={180} alt="Pokeball" />
                 </button>
 
                 {/* Navbar */}
-                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                <nav className={`${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
                     <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
                         {navigationElems.map(item => (
                             <a
@@ -37,9 +46,17 @@ function Header() {
                         ))}
                     </div>
                 </nav>
+                <button className='ml-auto lg:hidden px-3' onClick={toggleNavigation}>
+                    <div className={`${openNavigation ? 'hidden' : 'flex'}`}>
+                        ☰
+                    </div>
+                    <div className={`${openNavigation ? 'flex' : 'hidden'}`}>
+                        ⨉
+                    </div>
+                </button>
             </div>
         </div>
-  )
+    )
 }
 
 export default Header;
